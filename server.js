c65b500f9875bfe5f36ba94e56923715e6613eb5
Isaac Elesia <isaacelesia@gmail.com>
2020-04-28 15:23:48 -0500

    working on code

--- server.js
+++ server.js
@@ -1,16 +1,63 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const db = require('./db');
+require('dotenv').config();
 
 const app = express();
 
+const API_token = process.env.API_Token;
+
+const auth = (req, res, next) => {
+	if (!req.get('Authorization')) {
+		return res.json('Missing credentcails');
+	}
+
+	const key = req.get('Authorization').split(' ')[1];
+
+	if (!(key === API_token)) {
+		return res.json('Invalid token');
+	}
+
+	next();
+};
+
 //Middlewares
 app.use(cors());
+app.use(auth);
 app.use(morgan('common'));
 
 //Routes
 app.get('/movie', (req, res) => {
-	res.json('Hello their');
+	const { genre, country, avg_vote } = req.query;
+
+	let movies = [...db];
+
+	if (genre || country || avg_vote) {
+		if (genre) {
+			let search = genre.toLowerCase();
+			movies = movies.filter((movie) =>
+				movie.genre.toLowerCase().includes(search)
+			);
+			return res.json(movies);
+		}
+
+		if (country) {
+			let search = country.toLowerCase();
+			movies = movies.filter((movie) =>
+				movie.country.toLowerCase().includes(search)
+			);
+			return res.json(movies);
+		}
+
+		if (avg_vote) {
+			let search = parseInt(avg_vote);
+			movies = movies.filter((movie) => Number(movie.avg_vote) >= search);
+			return res.json(movies);
+		}
+	}
+
+	res.json(movies);
 });
 
 app.listen(5000, () => {
